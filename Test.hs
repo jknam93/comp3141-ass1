6d0e05606e36da360e405772e470cb93716bce18
Justin Nam <justin.k.nam@gmail.com>
2016-04-27 16:01:21 +1000

    Update Test.hs

--- Test.hs
+++ Test.hs
@@ -1,4 +1,15 @@
 module Test where
 
 import Test.QuickCheck
+import Simulation
+import Physics
+import World
+import TestSupport
+
+prop_EnergyConservation:: World -> Bool
+prop_EnergyConservation w =  ratio > 0.999 && ratio < 1.001
+   where
+      energy   = worldEnergy w + 1
+      energy'  = worldEnergy (advanceWorld 1 0.001 w) + 1
+      ratio    = abs (realToFrac (energy / energy'))
 
